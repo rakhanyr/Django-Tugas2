9cd55eb98cbd91f459e2782d4d27f779807d03e8
rakhanyr <rakhanyr@gmail.com>
2022-09-15 10:49:47 +0700

    Menambahkan variable 'name' dan 'npm'

--- katalog/views.py
+++ katalog/views.py
@@ -5,6 +5,7 @@ def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
         'list_barang': data_barang_katalog,
-        'nama': 'Rakhan Yusuf'
+        'nama': 'Rakhan Yusuf R.',
+        'npm' : '2106751852'
         }
     return render(request, "katalog.html", context)
