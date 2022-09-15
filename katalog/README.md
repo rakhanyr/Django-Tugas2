432eab2c32ff2685f373fa912c513c9f0c17211e
rakhanyr <rakhanyr@gmail.com>
2022-09-15 09:56:33 +0700

    Berhasil di run

---
+++ katalog/README.md
@@ -0,0 +1,5 @@
+
+
+
+
+virtual environment (lingkungan virtual) berfungsi untuk memisahkan pengaturan dan package yang diinstal pada setiap proyek Django sehingga perubahan yang dilakukan pada satu proyek tidak mempengaruhi proyek lainnya. Dengan kata lain, setiap proyek Django sebaiknya memiliki virtualenv-nya sendiri.
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Rakhan Yusuf'
+        }
+    return render(request, "katalog.html", context)
