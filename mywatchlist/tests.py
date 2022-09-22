046a918c3f784395f5a222649291e325876de7fe
rakhanyr <rakhanyr@gmail.com>
2022-09-22 11:36:38 +0700

    unittesting

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from mywatchlist.models import WatchList
 
 # Create your tests here.
+class UnitTesting(TestCase):
+    def test_app_url_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+class AppTest(TestCase):
+    def test_app_url_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+class AppTest(TestCase):
+    def test_app_url_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
